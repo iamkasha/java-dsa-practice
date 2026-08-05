3d4966a1d0d312215f32f7f5d0d9376f16abc54f
iamkasha <135172227+iamkasha@users.noreply.github.com>
2026-08-04 22:28:39 -0700

    Time: 1 ms (99.97%), Space: 46.4 MB (67.9%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        
-        Map<Character, Integer> magazineMap = new HashMap<>();
-        for(char c: magazine.toCharArray()){
-            magazineMap.put(c, magazineMap.getOrDefault(c, 0)+1);
+        int[] count = new int[26];
+        for (char c : magazine.toCharArray()) {
+            count[c - 'a']++;
         }
-
-        for(char c: ransomNote.toCharArray()){
-
-            if(!magazineMap.containsKey(c) || magazineMap.get(c) == 0){
-                return false;
-            }
-            magazineMap.put(c, magazineMap.get(c)-1);
+        for (char c : ransomNote.toCharArray()) {
+            count[c - 'a']--;
+            if (count[c - 'a'] < 0) return false;
         }
         return true;
     }
