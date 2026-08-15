b574a31ed47f50ed0c34a285788f99fc7d683dfd
iamkasha <135172227+iamkasha@users.noreply.github.com>
2026-08-15 11:56:17 -0700

    Time: 3 ms (52.16%), Space: 46.5 MB (57.25%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         
-        Set<Character> r[] = new HashSet[9];
-        Set<Character> c[] = new HashSet[9];
-        Set<Character> box[] = new HashSet[9];
+        //Resolve attempt
+        Set<Character> rowSet[] = new Set[9];
+        Set<Character> colSet[] = new Set[9];
+        Set<Character> box[] = new Set[9];
+
         for(int i=0; i<9; i++){
-            r[i] = new HashSet<>();
-            c[i] = new HashSet<>();
+            rowSet[i] = new HashSet<>();
+            colSet[i] = new HashSet<>();
             box[i] = new HashSet<>();
         }
 
@@ -15,14 +17,15 @@ class Solution {
                 if(board[i][j] == '.'){
                     continue;
                 }
-                int boxIndex = (i/3) * 3 + (j/3);
-                if(r[i].contains(board[i][j]) || c[j].contains(board[i][j]) || box[boxIndex].contains(board[i][j])){
+                int boxIndex = i/3 * 3 + j/3;
+                if(rowSet[i].contains(board[i][j]) || colSet[j].contains(board[i][j]) || box[boxIndex].contains(board[i][j])){
                     return false;
                 }
-                r[i].add(board[i][j]);
-                c[j].add(board[i][j]);
+                rowSet[i].add(board[i][j]);
+                colSet[j].add(board[i][j]);
                 box[boxIndex].add(board[i][j]);
             }
+
         }
         return true;
     }
